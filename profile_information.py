6df2dd1b982c81a3a6b173b1d784e11b9dcd46d8
Carl Angelo Amoroto <carlamoroto200526@gmail.com>
2024-11-21 00:19:34 +0800

    Writing in text file and arranging try and except

--- profile_information.py
+++ profile_information.py
@@ -18,9 +18,28 @@ with open(".\\user_information.txt", "a") as profile_information:
             dream_destination = input('Where do you want to travel? : ')
             biggest_fear = input('What are you scared the most? : ')
             life_motto = input('What is your life motto? : ')
+            
+                                    
+        
+        # Writing informations in text file
+            profile_information.write(f'Name: {name}\n')
+            profile_information.write(f'Age: {age}\n')
+            profile_information.write(f'Gender: {gender}\n')
+            profile_information.write(f'Address: {address}\n')
+            profile_information.write(f'Email: {email}\n')
+            profile_information.write(f'Hobbies: {hobbies}\n')
+            profile_information.write(f'Personality: {personality}\n')
+            profile_information.write(f'Religion: {religion}\n')
+            profile_information.write(f'Language: {language}\n')
+            profile_information.write(f'Favorite number: {favorite_number}\n')
+            profile_information.write(f'Dream career: {dream_career}\n')
+            profile_information.write(f'Dream destination: {dream_destination}\n')
+            profile_information.write(f'Biggest_fear: {biggest_fear}\n')
+            profile_information.write(f'Life motto: {life_motto}\n')
             break
+
         except ValueError:
-            print('Invalid')                             
+         print('Invalid') 
 
 # Ask user if want to input info again, exit if not
      
