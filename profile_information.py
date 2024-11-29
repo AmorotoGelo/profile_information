2ce26b479c1c16b090c2bb0227bcf68728d74902
Carl Angelo Amoroto <carlamoroto200526@gmail.com>
2024-11-29 20:27:37 +0800

    Adding twist in inputting info, removed some questions/variable

--- profile_information.py
+++ profile_information.py
@@ -10,13 +10,23 @@ with open(".\\user_information.txt", "a") as profile_information:
                   break
                except ValueError:
                   print("Invalid, Please try again")
-            gender = input('What is your gender identity? : ')
+
+            gender = input('Type xy if you are a man, xx if a woman : ')
+            if gender == 'xy':
+               gender = 'Man'
+            elif gender == 'xx':
+               gender = 'Woman'   
+
             address = input('Where do you live? : ')
-            email = input('What is your email? : ')
             hobbies = input('What are your hobbies? : ')
-            personality = input('Are you introvert or extrovert? : ')
+
+            personality = input('Type shy if you are introvert, loud if extrovert : ')
+            if personality == 'shy':
+               personality = 'Introvert'
+            elif personality == 'loud':
+               personality = 'Extrovert'
+               
             religion = input('What is your religious belief? : ')
-            language = input('What languages do you speak? : ')
             nationality = input('What is your nationality? : ')
             while True:
                try:
@@ -25,9 +35,9 @@ with open(".\\user_information.txt", "a") as profile_information:
                except ValueError:
                   print("Invalid, Please try again")     
             dream_career = input('What is your dream job? : ')
-            dream_destination = input('Where do you want to travel? : ')
-            biggest_fear = input('What are you scared the most? : ')
-            life_motto = input('What is your life motto? : ')
+            
+            
+            
             
                                     
         
@@ -36,16 +46,11 @@ with open(".\\user_information.txt", "a") as profile_information:
             profile_information.write(f'Age: {age}\n')
             profile_information.write(f'Gender: {gender}\n')
             profile_information.write(f'Address: {address}\n')
-            profile_information.write(f'Email: {email}\n')
             profile_information.write(f'Hobbies: {hobbies}\n')
             profile_information.write(f'Personality: {personality}\n')
             profile_information.write(f'Religion: {religion}\n')
-            profile_information.write(f'Language: {language}\n')
             profile_information.write(f'Favorite number: {favorite_number}\n')
             profile_information.write(f'Dream career: {dream_career}\n')
-            profile_information.write(f'Dream destination: {dream_destination}\n')
-            profile_information.write(f'Biggest fear: {biggest_fear}\n')
-            profile_information.write(f'Life motto: {life_motto}\n')
             profile_information.write("\n \n")
         
 
